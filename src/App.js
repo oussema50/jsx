b6fd5ceeecb042909096b17d61188272a2e17704
oussema50 <omranioussema3@gmail.com>
2021-08-26 17:10:19 +0100

    checkpoint jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import './style.css';
+import imageInSrc from './imageInSrc.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div>
+      
+        <h1 className="title red">oussema omrani</h1><br/>
+        <img src={imageInSrc} alt="img"/><br/>        
+        <img src={window.location.origin + '/imageInPublic.jpg'} alt="img"/><br/>        
+      </div>
+      <iframe  src="https://www.youtube.com/embed/Ke90Tje7VS0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
+    
   );
 }
 
